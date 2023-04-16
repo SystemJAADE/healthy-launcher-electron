0090d5c7e64e3c0af16014e8fb555f1908d8623a
elitgamaliel <elitgamaliel@gmail.com>
2023-04-16 10:53:23 -0500

    Update FIle recent

--- main.js
+++ main.js
@@ -1,41 +1,41 @@
-const path = require('path');
-const { app, BrowserWindow } = require('electron')
+const path = require("path");
+const { app, BrowserWindow } = require("electron");
 
 // Crear la ventana principal sin marco y sin barra de menú
-function createWindow () {
+function createWindow() {
   const mainWindow = new BrowserWindow({
-    width: 510,
+    // width: 510,
+    // height: 430,
+    width: 900,
     height: 430,
     frame: false, // para eliminar la barra de título
     autoHideMenuBar: true, // para ocultar la barra de menú
+    icon: path.join(__dirname, "/Healthy.ico"), // icono de la aplicacion
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false
-    }
-  })
+      contextIsolation: false,
+      // Configurar la política de seguridad de contenido
+      contentSecurityPolicy: "default-src 'self' https://systemjaade.com",
+    },
+  });
 
   // Cargar el archivo HTML de la aplicación
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile("index.html");
+  mainWindow.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow();
     }
-  })
+  });
+});
 
-})
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
-
-app.whenReady().then(() => {
-  const iconPath = path.join(__dirname, 'images/Healthy.ico');
-  app.dock.setIcon(iconPath);
 });
--- script.js
+++ script.js
@@ -1,8 +1,105 @@
-const remote = require("electron").remote;
+const { app } = require("electron");
+const { Client } = require("basic-ftp");
+const fs = require("fs");
+const path = require("path");
+const { exec } = require("child_process");
 
-const closeAppBtn = document.querySelector(".close-link");
-
-closeAppBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-  window.close();
+window.addEventListener("DOMContentLoaded", () => {
+  // Nueva llamada a la función para listar los archivos FTP
+  loadFtpFiles();
 });
+
+async function loadFtpFiles() {
+  await listFtpFiles();
+  vaildateJarExistence();
+  // downloadLatestFile();
+}
+
+async function listFtpFiles() {
+  const client = new Client();
+  let fileNames = [];
+  try {
+    await client.access({
+      host: "ftp.systemjaade.com",
+      port: "21",
+      user: "healthy-ant@systemjaade.com",
+      password: "healthy.2020",
+    });
+
+    const files = await client.list("/Healthy");
+    fileNames = files.map((file) => file.name);
+    const recentFile = findMostRecentFile(fileNames);
+    console.log("recent: ", recentFile);
+
+    const dir = path.join(__dirname, "Healthy");
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+    console.log('dir',dir);
+    
+    const localFilePath = path.join(__dirname, "Healthy", recentFile);
+    await client.downloadTo(localFilePath, recentFile);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    client.close();
+  }
+}
+
+function findMostRecentFile(fileNames) {
+  let mostRecentFile = null;
+  let mostRecentDate = new Date(0);
+
+  fileNames.forEach((fileName) => {
+    const dateMatch = fileName.match(/(\d{2})\.(\d{2})\.(\d{2})/);
+
+    if (dateMatch) {
+      const year = parseInt(`20${dateMatch[1]}`);
+      const month = parseInt(dateMatch[2]) - 1; // meses en objetos Date van de 0 a 11
+      const day = parseInt(dateMatch[3]);
+      const fileDate = new Date(year, month, day);
+
+      if (fileDate > mostRecentDate) {
+        mostRecentDate = fileDate;
+        mostRecentFile = fileName;
+      }
+    }
+  });
+
+  return mostRecentFile;
+}
+
+function vaildateJarExistence() {
+  // Ruta de la carpeta "Healthy"
+  const healthyFolderPath = path.join(__dirname, "Healthy");
+
+  // Leer la carpeta "Healthy" para encontrar un archivo .jar
+  fs.readdir(healthyFolderPath, (err, files) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    // Encontrar el primer archivo .jar en la carpeta
+    const jarFile = files.find((file) => path.extname(file) === ".jar");
+
+    if (!jarFile) {
+      console.log("No se encontró ningún archivo .jar en la carpeta Healthy.");
+      return;
+    }
+
+    // Ejecutar el comando "java -jar nombre-del-archivo.jar -version" en una terminal
+    const command = `java -jar "${path.join(
+      healthyFolderPath,
+      jarFile
+    )}" -version`;
+    exec(command, (err, stdout, stderr) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      // console.log(stdout); // imprime version actual en sistema
+      currentVersion = stdout;
+    });
+  });
+}
